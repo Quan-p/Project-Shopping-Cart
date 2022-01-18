ab617cfa4ca8330b9852555f57e4a9a6301ad987
Quan-p <quanp155@gmail.com>
2022-01-18 16:18:08 -0600

    Added case images and object data

---
+++ src/assets/caseData.js
@@ -0,0 +1,41 @@
+import vk3 from './images/cases/vk3.jpg';
+import formd from './images/cases/formd.png';
+import mesh from './images/cases/mesh.png';
+import ncase from './images/cases/ncase.png';
+
+const cases = [
+    {
+        category: 'Cases',
+        categoryId: 'cases',
+        id: 'vk3',
+        name: 'Velka 3',
+        price: '$159.99',
+        image: vk3,
+    },
+    {
+        category: 'Cases',
+        categoryId: 'cases',
+        id: 'formd',
+        name: 'FormD T1',
+        price: '$175.00',
+        image: formd,
+    },
+    {
+        category: 'Cases',
+        categoryId: 'cases',
+        id: 'mesh',
+        name: 'SSUPD Meshlicious',
+        price: '$109.99',
+        image: mesh,
+    },
+    {
+        category: 'Cases',
+        categoryId: 'cases',
+        id: 'ncase',
+        name: 'NCASE M1',
+        price: '$200.00',
+        image: ncase,
+    },
+];
+
+export default cases;
