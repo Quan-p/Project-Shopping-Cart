38ca6dc673f8ed5f6c3fbe63989017a6920af592
Quan-p <quanp155@gmail.com>
2022-01-19 16:28:17 -0600

    Created increment decrement component for items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Homepage from './pages/Homepage';
 import './css/App.css'
+import Counter from './components/Counter';
 
 const App = () => {
   return (
     <div className = 'home'>
       <Homepage />
+      <Counter />
     </div>
   )
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+    let [num, setNum] = useState(1);
+
+    let incNum = () => {
+        if (num < 10) {
+            setNum(Number(num) + 1);
+        }
+    };
+    let decNum = () => {
+        if (num > 0) {
+            setNum(num - 1);
+        }
+    };
+    let handleChange = (e) => {
+        setNum(e.target.value)
+    };
+
+    return (
+        <>
+            <div>
+                <button onClick={decNum}>
+                    down
+                </button>
+            </div>
+            <input type='text' onChange={handleChange} value={num}/>
+            <div>
+                <button onClick={incNum}>
+                    up
+                </button>
+            </div>
+        </>
+    );
+};
+
+export default Counter;
