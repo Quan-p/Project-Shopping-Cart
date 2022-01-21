97d9c2e36582a4b8991ec7dcee44674238a54bd6
Quan-p <quanp155@gmail.com>
2022-01-20 18:00:54 -0600

    Removed old code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import Homepage from './pages/Homepage';
 import './css/App.css'
-import Counter from './components/Counter';
 
 const App = () => {
   return (
     <div className = 'home'>
       <Homepage />
-      <Counter />
     </div>
   )
 }
