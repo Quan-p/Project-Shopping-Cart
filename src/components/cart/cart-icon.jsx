85010541880903251f6562e809cd9c963c500406
Quan-p <quanp155@gmail.com>
2022-01-22 16:34:49 -0600

    Add and style cart counter

---
+++ src/components/cart/cart-icon.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ShoppingCartOutlined } from '@material-ui/icons';
+import './cart-icon.styles.scss';
+
+const CartIcon = () => {
+    return (
+        <div className='cart-container'>
+            <ShoppingCartOutlined className = 'cart-icon' alt='shopping-cart-icon'></ShoppingCartOutlined>
+            <span className='cart-count'>5</span>
+        </div>
+    );
+}
+
+export default CartIcon;
