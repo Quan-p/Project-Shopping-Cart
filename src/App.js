f53418ef1da2dc0e1bd72fc0650902a4798c6fed
Quan-p <quanp155@gmail.com>
2022-01-24 14:25:19 -0600

    Imported new hero image and styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import Header from './components/header/header';
+import Hero from './components/hero/hero';
 import './css/App.scss'
 
 const App = () => {
   return (
     <div className = 'home'>
       <Header />
+      <Hero />
     </div>
   )
 }
