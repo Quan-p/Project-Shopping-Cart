1ff306360bf09e02ce61faa51b83fedf910e769e
Quan-p <quanp155@gmail.com>
2022-01-24 16:02:53 -0600

    Added sff description

--- src/components/main-section/main-section.jsx
+++ src/components/main-section/main-section.jsx
@@ -10,7 +10,11 @@ const MainSection = ({ history }) => {
                     <img src= {main} alt='case' />
                 </div>
                 <div className = 'ms-m-description'>
-                    <h2>Designed for portability and modularity.</h2>
+                    <h2>Decreasing size and maximizing space efficiency.</h2>
+                    <p>
+                    Intended to minimize the volume and footprint of a desktop computer 
+                    compared to the standard ATX form factor.
+                    </p>
                 </div>
             </div>
         </div>
