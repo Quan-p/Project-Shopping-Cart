8cdb89ac42e15a6d74aef3f52140f789fc5dbdb2
Quan-p <quanp155@gmail.com>
2022-01-24 15:46:38 -0600

    Moved case data info to new index.js

---
+++ src/shop/index.js
@@ -0,0 +1,35 @@
+import vk3 from '../assets/images/cases/vk3.jpg';
+import formd from '../assets/images/cases/formd.png';
+import mesh from '../assets/images/cases/mesh.png';
+import ncase from '../assets/images/cases/ncase.png';
+
+const SHOP_DATA = [
+    {
+        id: '1',
+        title: 'Velka 3',
+        description: 'This case is ideal for those who want the smallest physically possible gaming PC. Made from thick steel and aluminum panels to give each part and the complete build a solid feel.',
+        price: 159.9,
+        src: vk3,
+    },
+    {
+        id: '2',
+        title: 'FormD T1',
+        description: 'A portable and modular PC enclosure that can be assembled into three different configurations, allowing for better compatibility, thermal, and space optimization.',
+        price: 175.00,
+        src: formd,
+    },
+    {
+        id: '3',
+        title: 'SSUPD Meshlicious',
+        description: 'Meshlicious is a combination of MESH and Delicious. There’s so much mesh that your mouth will water. Breathable, mesh panels lets you savor the airflow and minty temps.',
+        price: 109.99,
+        src: mesh,
+    },
+    {
+        id: '4',
+        title: 'NCASE M1',
+        description: 'With its sleek, subtly-faceted brushed aluminum front panel, the M1 presents a refined, minimalist face to the world.',
+        price: 200.00,
+        src: ncase,
+    },  
+]
