f87408987967268804f9f7b30cf0e34840270688
Quan-p <quanp155@gmail.com>
2022-01-25 16:55:22 -0600

    Moved separate components to one homepage with react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, { useState } from 'react';
-import Footer from './components/footer/footer';
-import Header from './components/header/header';
-import Hero from './components/hero/hero';
-import MainSection from './components/main-section/main-section';
+import { BrowserRouter, Routes, Route, Outlet} from "react-router-dom";
+import HomePage from "./components/home-page";
 import './css/App.scss'
 
 const App = () => {
   return (
-    <div className = 'home'>
-      <Header />
-      <Hero />
-      <MainSection />
-      <Footer />
+    <div className = 'App'>
+      <Routes>
+        <Route exact path='/' element = {<HomePage/>}></Route>
+      </Routes>
     </div>
   )
 }
