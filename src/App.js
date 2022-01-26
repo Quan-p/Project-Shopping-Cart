c879e90d9645f6583445de5cb493fdb2e35c7df0
Quan-p <quanp155@gmail.com>
2022-01-26 15:33:50 -0600

    Set up single product page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { BrowserRouter, Routes, Route, Outlet} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./components/home-page";
 import Shop from './components/pages/shop/shop'
+import SingleProduct from "./components/single-product/single-product";
 import './css/App.scss'
 
 const App = () => {
   return (
     <div className = 'App'>
-      <Routes>
-        <Route exact path='/' element = {<HomePage/>}></Route>
-        <Route path='/shop' element = {<Shop/>}></Route>
-      </Routes>
+      <Switch>
+        <Route exact path='/' component = {HomePage} />
+        <Route path='/shop' component = {Shop} />
+        <Route path='/product/:id' component= {SingleProduct} />
+      </Switch>
     </div>
   )
 }
---
+++ src/components/single-product/single-product.jsx
@@ -0,0 +1,55 @@
+import React, { useContext, useState, useEffect } from "react";
+import { withRouter } from 'react-router-dom';
+import { ProductsContext } from "../../context/products-context";
+import Layout from "../shared/layout";
+import './single-product.styles.scss'
+
+const SingleProduct = ({ match, history: { push} }) => {
+    const { products } = useContext(ProductsContext);
+    const { id } = match.params;
+    const [product, setProduct] = useState(null);
+    useEffect(() => {
+        const product = products.find(item => Number(item.id) === Number(id))
+
+        if (!product) {
+            return push('/shop')
+        }
+
+        setProduct(product);
+    }, [id, product, push, products]);
+    //while checking for product return null
+    if (!product) { return null }
+
+    const{ src, title, price, description } = product;
+
+    return (
+        <Layout>
+            <div className='single-product-container'>
+                <div className='product-image'>
+                    <img src = {src} alt='product' />
+                </div>
+                <div className='product-details'>
+                    <div className='name-price'>
+                        <h3>{title}</h3>
+                        <p>{price}</p>
+                    </div>
+                    <div className='add-to-cart-btns'>
+                        <button className='button is-white nomad-btn' id='btn-white-outline'>
+                            ADD TO CART
+                        </button>
+                        <button className='button is-black nomad-btn'>
+                            PROCEED TO CHECKOUT
+                        </button>
+                    </div>
+                    <div className='product-description'>
+                        <p>
+                            {description}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+}
+
+export default withRouter(SingleProduct);
