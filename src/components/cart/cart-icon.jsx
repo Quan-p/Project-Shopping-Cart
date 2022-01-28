ec89320be81631cc912d347f2e47cbeffdfc7216
Quan-p <quanp155@gmail.com>
2022-01-28 16:49:59 -0600

    Add onclick function to cart icon to route to cart

--- src/components/cart/cart-icon.jsx
+++ src/components/cart/cart-icon.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
+import { withRouter } from "react-router";
 import { ShoppingCartOutlined } from '@material-ui/icons';
 import { CartContext } from "../../context/cart-context";
 import './cart-icon.styles.scss';
 
-const CartIcon = () => {
+const CartIcon = ({ history }) => {
     const { itemCount, cartItems } = useContext(CartContext);
     console.log('CartItems:', cartItems);
     return (
-        <div className='cart-container'>
+        <div className='cart-container' onClick={() => history.push('./cart')}>
             <ShoppingCartOutlined className = 'cart-icon' alt='shopping-cart-icon' />
             {
                 itemCount > 0 ? <span className='cart-count'> { itemCount } </span> : null
@@ -16,4 +17,4 @@ const CartIcon = () => {
     );
 }
 
-export default CartIcon;
+export default withRouter(CartIcon);
